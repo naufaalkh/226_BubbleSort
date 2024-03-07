db33e27ef7bca6ca6c8b29e5ff108bf16bd2ce39
naufaalkh <naufaalkh@gmail.com>
2024-03-07 19:10:39 +0700

    Memanggil Prosedur

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -47,3 +47,24 @@ void bubbleSortArray()
     } while (pass < n - 1); // step 5
 }
 
+void display()
+{
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Elemen Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl; // output setiap element array pada garis baru
+    }
+    cout << "jumlah pass = " << n - 1 << endl;
+    cout << endl;
+}
+
+int main()
+{
+    input();
+    bubbleSortArray();
+    display();
+    system("pause");
+}
