794346290bc1135c5bc72f719059dc73fa33ac72
naufaalkh <naufaalkh@gmail.com>
2024-03-07 19:04:25 +0700

    Prosedur proses bubble sort

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -32,4 +32,15 @@ void bubbleSortArray()
 {                 // procedur untuk mengurutkan array
     int pass = 1; // step 1
     do
+    {
+        for (int j = 0; j <= n - 1 - pass; j++)
+        { // step 2
+            if (arr[j] > arr[j + 1])
+            { // step 3
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
